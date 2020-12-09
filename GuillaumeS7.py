b391ff9d251ecb94f2ecc45a5eb8cd93ba727b3d
Guillaume <guideb21@orange.fr>
2020-12-09 16:54:35 +0100

    fixx max options

--- GuillaumeS7.py
+++ GuillaumeS7.py
@@ -1,6 +1,6 @@
 from pycsp3 import *
 
-options = LD, SE, BIO = 1, 2, 3
+options = LD, SE, BIO = 0, 1, 2
 optionsPlaces = [4, 4, 2]
 Guillaume, Maxime, Alban, Roger, Manu, Vegeta, Morgane, Clara = eleves = VarArray(size=8, dom=options)
 
@@ -10,7 +10,7 @@ coefsOptions = [[2, 1], [1, 1], [1, 3]]
 
 elevesMoyenneOptions = [[], [], [], [], [], [], [], []]
 
-elevesChoix = [[0, 1, 2], [1, 0, 2], [0, 1, 2], [2, 1, 0], [1, 2, 0], [0, 2, 1], [1, 0, 2], [0, 1, 2]]
+elevesChoix = [[0, 1, 2], [2, 1, 2], [2, 1, 2], [2, 1, 0], [1, 2, 0], [0, 2, 1], [1, 0, 2], [0, 1, 2]]
 
 for y in range(len(options)):
     for i in range(len(eleves)):
@@ -20,11 +20,15 @@ for y in range(len(options)):
         moyenne = round(moyenne / sum(coefsOptions[y]), 2)
         elevesMoyenneOptions[i].append(moyenne)
 
+print(1==1)
+print(1==2)
+print(1==1 or 1==2)
 
 satisfy(
     # Taille max par options
     [Count(eleves, value=options[i]) <= optionsPlaces[i] for i in range(len(options))],
 
+    [(eleves[i]==options[elevesChoix[i][0]]) or (Count(eleves, value=options[elevesChoix[i][0]]) == optionsPlaces[elevesChoix[i][0]]) for i in range(len(eleves))]
 
 
 
