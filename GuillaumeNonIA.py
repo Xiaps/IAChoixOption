4a9779c8fb0c112cad4052ddc23f92e64527166a
Guillaume <guideb21@orange.fr>
2020-12-26 15:30:41 +0100

    Version perso, version perso V2 (celle du prof actuellement) et version normalment fonctionnelle sans utilisation d'IA

---
+++ GuillaumeNonIA.py
@@ -0,0 +1,147 @@
+#Les options
+options = LD, SE, BIO = 0, 1, 2
+
+#Les places de optionss
+optionsPlaces = [4, 4, 2]
+
+#Les eleves
+eleves = "Guillaume", "Maxime", "Alban", "Roger", "Manu", "Morgane", "Vegeta", "Clara"
+
+#Set de choix pour chaque eleve
+elevesChoix = [[2, 1, 0], [2, 0, 2], [2, 1, 0], [1, 2, 0], [1, 0, 2], [0, 2, 1], [1, 0, 2], [1, 0, 2]]
+
+#La suite n'est pas encore utilise (jusqu'au satisfy)
+
+#Set de notes pour chaque eleve
+elevesNotes = [[15, 15], [12, 18], [20, 4], [12, 13], [9, 8], [17, 18], [5, 19], [16, 11]]
+
+#Set de coefs des notes pour chaque option
+coefsOptions = [[2, 1], [1, 1], [1, 3]]
+
+#Set de moyennes coeficientés par option pour chaque eleve
+elevesMoyenneOptions = [[], [], [], [], [], [], [], []]
+
+#Calcul des moyennes coeficcientees pour chaque eleve
+for y in range(len(options)):
+    for i in range(len(eleves)):
+        moyenne = 0
+        for x in range(len(elevesNotes[0])):
+            moyenne += elevesNotes[i][x] * coefsOptions[y][x]
+        moyenne = round(moyenne / sum(coefsOptions[y]), 2)
+        elevesMoyenneOptions[i].append(moyenne)
+
+elevesFull=[]
+for i in range(len(eleves)) :
+    eleveData = []
+    eleveData.append(eleves[i])
+    eleveData.append(elevesMoyenneOptions[i])
+    eleveData.append(elevesChoix[i])
+    elevesFull.append(eleveData)
+
+elevesRestant = elevesFull.copy()
+optionsRemplis = [[],[],[]]
+
+#1er tour
+for i in range(len(elevesFull)) :
+    optionsRemplis[elevesFull[i][2][0]].append(elevesFull[i])
+    elevesRestant.remove(elevesFull[i])
+
+#On classe dans l'ordre des moyennes par options :
+
+for op in range(len(options)) :
+    n = len(optionsRemplis[op])
+    #    Traverser tous les éléments du tableau
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            # échanger si l'élément trouvé est plus grand que le suivant
+            if optionsRemplis[op][j][1][op] < optionsRemplis[op][j+1][1][op] :
+                optionsRemplis[op][j], optionsRemplis[op][j + 1] = optionsRemplis[op][j + 1], optionsRemplis[op][j]
+
+print("=====LD======")
+print(optionsRemplis[0])
+print("=====SE======")
+print(optionsRemplis[1])
+print("=====BIO======")
+print(optionsRemplis[2])
+#On coupe :
+for i in range(len(optionsRemplis)) :
+    while len(optionsRemplis[i]) > optionsPlaces[i] :
+        elevesRestant.append(optionsRemplis[i].pop())
+
+print("=====RESTANT======")
+print(elevesRestant)
+
+elevesRestantAvant = []
+index=0
+
+#Second choix
+while(len(elevesRestant)!=0 and elevesRestantAvant!=elevesRestant) :
+    elevesRestantAvant = elevesRestant.copy()
+
+    for index in range(len(elevesRestantAvant)) :
+        optionsRemplis[elevesRestantAvant[index][2][1]].append(elevesRestantAvant[index])
+        elevesRestant.remove(elevesRestantAvant[index])
+
+    # On classe dans l'ordre des moyennes par options :
+
+    for op in range(len(options)):
+        n = len(optionsRemplis[op])
+        #    Traverser tous les éléments du tableau
+        for i in range(n):
+            for j in range(0, n - i - 1):
+                # échanger si l'élément trouvé est plus grand que le suivant
+                if optionsRemplis[op][j][1][op] < optionsRemplis[op][j + 1][1][op]:
+                    optionsRemplis[op][j], optionsRemplis[op][j + 1] = optionsRemplis[op][j + 1], optionsRemplis[op][j]
+
+    # On coupe :
+    for i in range(len(optionsRemplis)):
+        while len(optionsRemplis[i]) > optionsPlaces[i]:
+            elevesRestant.append(optionsRemplis[i].pop())
+
+
+
+print("___________2EME_TOUR______________")
+print("=====LD======")
+print(optionsRemplis[0])
+print("=====SE======")
+print(optionsRemplis[1])
+print("=====BIO======")
+print(optionsRemplis[2])
+print("=====RESTANT======")
+print(elevesRestant)
+
+#Troisieme choix
+while(len(elevesRestant)!=0 and elevesRestantAvant!=elevesRestant) :
+    elevesRestantAvant = elevesRestant.copy()
+
+    for index in range(len(elevesRestantAvant)) :
+        optionsRemplis[elevesRestantAvant[index][2][2]].append(elevesRestantAvant[index])
+        elevesRestant.remove(elevesRestantAvant[index])
+
+    # On classe dans l'ordre des moyennes par options :
+
+    for op in range(len(options)):
+        n = len(optionsRemplis[op])
+        #    Traverser tous les éléments du tableau
+        for i in range(n):
+            for j in range(0, n - i - 1):
+                # échanger si l'élément trouvé est plus grand que le suivant
+                if optionsRemplis[op][j][1][op] < optionsRemplis[op][j + 1][1][op]:
+                    optionsRemplis[op][j], optionsRemplis[op][j + 1] = optionsRemplis[op][j + 1], optionsRemplis[op][j]
+
+    # On coupe :
+    for i in range(len(optionsRemplis)):
+        while len(optionsRemplis[i]) > optionsPlaces[i]:
+            elevesRestant.append(optionsRemplis[i].pop())
+
+
+
+print("___________3EME_TOUR______________")
+print("=====LD======")
+print(optionsRemplis[0])
+print("=====SE======")
+print(optionsRemplis[1])
+print("=====BIO======")
+print(optionsRemplis[2])
+print("=====RESTANT======")
+print(elevesRestant)
--- GuillaumeS7V2.py
+++ GuillaumeS7V2.py
@@ -1,29 +1,20 @@
 from pycsp3 import *
-
 #Les options
 options = LD, SE, BIO = 0, 1, 2
-
 #Les places de optionss
 optionsPlaces = [4, 4, 2]
-
 #Les eleves
 Guillaume, Maxime, Alban, Roger, Manu, Morgane, Vegeta, Clara = eleves = VarArray(size=8, dom=options)
-
-varChoixObtenu = VarArray(size=8, dom=range(3))
-
-#Set de choix pour chaque eleve
-elevesChoix = [[5, 1, 0], [5, 1, 2], [5, 1, 2], [5, 2, 0], [5, 2, 0], [5, 2, 1], [5, 0, 2], [5, 0, 2]]
+choix = VarArray(size=8,dom=range(3))
+#Set de préférence pour chaque eleve
+elevesPrefs = [[2, 1, 0], [0, 1, 2], [0, 1, 2], [1, 2, 0], [1, 2, 0], [0, 2, 1], [1, 0, 2], [1, 0, 2]]
 #La suite n'est pas encore utilise (jusqu'au satisfy)
-
 #Set de notes pour chaque eleve
 elevesNotes = [[15, 15], [12, 18], [20, 4], [12, 13], [9, 8], [17, 18], [5, 19], [16, 11]]
-
 #Set de coefs des notes pour chaque option
 coefsOptions = [[2, 1], [1, 1], [1, 3]]
-
 #Set de moyennes coeficientés par option pour chaque eleve
 elevesMoyenneOptions = [[], [], [], [], [], [], [], []]
-
 #Calcul des moyennes coeficcientees pour chaque eleve
 for y in range(len(options)):
     for i in range(len(eleves)):
@@ -32,19 +23,15 @@ for y in range(len(options)):
             moyenne += elevesNotes[i][x] * coefsOptions[y][x]
         moyenne = round(moyenne / sum(coefsOptions[y]), 2)
         elevesMoyenneOptions[i].append(moyenne)
-
 #Conditions : (Commentaires hors du satisfy sinon erreur chez moi, raison inconnue)
-
 # Il ne peut pas y avoir plus d'eleves par option que de place disponible
 # Un eleve doit être attribue dans l'option de son 1er choix sauf si celle ci est pleine. (Donc soit il valide que son opption est son choix 1 soit que cette option est pleine.)
-
 satisfy(
     [Count(eleves, value=options[i]) <= optionsPlaces[i] for i in range(len(options))],
-    [varChoixObtenu[i]==elevesChoix[i].index(eleves[i]) for i in range(len(eleves))]
+     [choix[i]==elevesPrefs[i].index(eleves[i]) for i in range(len(eleves))]
 )
-
 minimize(
+    Sum(choix)
     #Sum(elevesChoix[i].index(eleves[i]) for i in range(len(eleves)))
-
-    Sum(varChoixObtenu)
+    #Sum(i for i in range(len(eleves)))
 )
