0502bbca355e5e63b3668edf820f7f94a731ee35
Maxime Belouin <maxime.belouin@reseau.eseo.fr>
2021-01-06 15:23:07 +0100

    Diagramme pie et export csv

--- visualisationDonnees.py
+++ visualisationDonnees.py
@@ -1,6 +1,8 @@
-#0 0 1 1 0 0 1 0 0 0 0 1 1 0 0 1 0 0 0 1 0 0 1 0 0 0 0 0 1 0 0 0 15 14 15 13 9 17 12 14 104
+#0 0 1 0 0 0 0 1 1 0 0 0 1 0 0 0 0 0 1 0 1 0 0 0 0 1 0 0 0 0 0 1 0 0 0 1 0 0 0 0 15 15 15 12 8 17 12 14 103
 
 import numpy as np;
+import pandas as pd;
+import matplotlib.pyplot as plt
 
 options =["LD","SE","BIO", "CSS"]
 eleve = ["Guillaume", "Maxime", "Alban", "Roger", "Manu", "Morgane", "Vegeta", "Clara"]
@@ -17,17 +19,17 @@ nbEleveParOption=[]
 for i in range(nombreOptions):
     nbEleveParOption.append(0)
 
-resultat=[[],[],[],[]]
+resultat=[["Prenom"],["Option attribuee"],["Rang du choix"],["Moyenne dans l'option"]]
 
 def convertChoix(choix):
     if(choix == "0"):
         return "Premier choix"
     elif(choix == "1"):
-        return "Deuxième choix"
+        return "Deuxieme choix"
     elif(choix == "2"):
-        return "Troisième choix"
+        return "Troisieme choix"
     elif(choix == "3"):
-        return "Quatrième choix"
+        return "Quatrieme choix"
     else : return "choix trop loin"
 
 for i in range(round(nombreEleve)):
@@ -39,13 +41,36 @@ for i in range(round(nombreEleve)):
             resultat[1].append(options[j])
             nbEleveParOption[j]+=1
 
-
-
 resultat= np.array(resultat)
 resultat=np.transpose(resultat)
 
 for i in range(nombreOptions):
     print("Il y a "+str(nbEleveParOption[i])+" étudiant dans l'option "+ options[i])
 print(resultat)
+
+df = pd.DataFrame(resultat)
+df.to_csv('choixEffectue.csv',index=False,header=False)
+
+nbPremierChoix=0
+nbDeuxiemeChoix=0
+nbTroisiemeChoix=0
+
+
+for i in range(len(choixObtenu)):
+    if choixObtenu[i]=="0":
+        nbPremierChoix+=1
+    elif choixObtenu[i]=="1":
+        nbDeuxiemeChoix+=1
+    elif choixObtenu[i]=="2":
+        nbTroisiemeChoix+=1
+
+print(nbPremierChoix)
+x = [nbPremierChoix,nbDeuxiemeChoix,nbTroisiemeChoix]
+plt.title("Proportion d'élève ayant eu leur premier choix")
+plt.pie(x, labels = ['Premier choix', 'Deuxième choix', 'Troisième choix'], normalize = True)
+plt.legend()
+plt.show()
+
+
 
 
