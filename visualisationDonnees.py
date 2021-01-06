2bb5cb549fd284b23245140f50a5899fa5eb2c48
Maxime Belouin <maxime.belouin@reseau.eseo.fr>
2021-01-06 14:32:47 +0100

    Amélioration de la visualisation des resultats

--- visualisationDonnees.py
+++ visualisationDonnees.py
@@ -2,16 +2,16 @@
 
 import numpy as np;
 
-options =["LD","SE","BIO"]
+options =["LD","SE","BIO", "CSS"]
 eleve = ["Guillaume", "Maxime", "Alban", "Roger", "Manu", "Morgane", "Vegeta", "Clara"]
 rawData = input();
 
 data = rawData.split(" ");
 nombreEleve = len(eleve);
 nombreOptions = len(options)
-optionChoisi=data[:nombreEleve*3]
-choixObtenu=data[nombreEleve*3:nombreEleve*3+8]
-notesOptions=data[nombreEleve*3+8:nombreEleve*3+16]
+optionChoisi=data[:nombreEleve*nombreOptions]
+choixObtenu=data[nombreEleve*nombreOptions:nombreEleve*nombreOptions+8]
+notesOptions=data[nombreEleve*nombreOptions+8:nombreEleve*nombreOptions+16]
 
 nbEleveParOption=[]
 for i in range(nombreOptions):
@@ -19,14 +19,26 @@ for i in range(nombreOptions):
 
 resultat=[[],[],[],[]]
 
+def convertChoix(choix):
+    if(choix == "0"):
+        return "Premier choix"
+    elif(choix == "1"):
+        return "Deuxième choix"
+    elif(choix == "2"):
+        return "Troisième choix"
+    elif(choix == "3"):
+        return "Quatrième choix"
+    else : return "choix trop loin"
+
 for i in range(round(nombreEleve)):
     resultat[0].append(eleve[i]);
-    resultat[2].append(choixObtenu[i])
-    resultat[3].append(notesOptions[i])
+    resultat[2].append(convertChoix(choixObtenu[i]))
+    resultat[3].append("Moyenne dans l'option : " +notesOptions[i])
     for j in range(nombreOptions):
         if(data[i*nombreOptions+j]== "1"):
             resultat[1].append(options[j])
             nbEleveParOption[j]+=1
+
 
 
 resultat= np.array(resultat)
