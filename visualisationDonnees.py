5a31819ba150dc0f9c3a967b408b03a954ddb99a
Maxime Belouin <maxime.belouin@reseau.eseo.fr>
2021-01-06 11:37:19 +0100

    Visualisation des resultats tableau 2

--- visualisationDonnees.py
+++ visualisationDonnees.py
@@ -12,20 +12,17 @@ notesOptions=data[nombreEleve*3+8:nombreEleve*3+16]
 resultat=[[],[],[],[]]
 
 for i in range(round(nombreEleve)):
-    #print(eleve[i]+" est dans l'option : ")
     resultat[0].append(eleve[i]);
     resultat[2].append(choixObtenu[i])
     resultat[3].append(notesOptions[i])
     if(data[i*3] == "1"):
-        #print("LD")
         resultat[1].append("LD")
     if (data[i * 3+1] == "1"):
-        #print("SE")
         resultat[1].append("SE")
     if (data[i * 3+2] == "1"):
-        #print("BIO")
         resultat[1].append("BIO")
 
-#resultat=np.array(resultat)
 resultat= np.array(resultat)
+resultat=np.transpose(resultat)
+
 print(resultat)
