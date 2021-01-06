97e9392ee3a1758c079acdf7f2ab58b8c028a5ac
Maxime Belouin <maxime.belouin@reseau.eseo.fr>
2021-01-06 12:06:29 +0100

    Visualisation des resultats tableau Adaptation pour plusieurs options

--- visualisationDonnees.py
+++ visualisationDonnees.py
@@ -1,13 +1,21 @@
+#0 0 1 1 0 0 1 0 0 0 0 1 1 0 0 1 0 0 0 1 0 0 1 0 0 0 0 0 1 0 0 0 15 14 15 13 9 17 12 14 104
+
 import numpy as np;
 
+options =["LD","SE","BIO"]
 eleve = ["Guillaume", "Maxime", "Alban", "Roger", "Manu", "Morgane", "Vegeta", "Clara"]
 rawData = input();
 
 data = rawData.split(" ");
 nombreEleve = len(eleve);
+nombreOptions = len(options)
 optionChoisi=data[:nombreEleve*3]
 choixObtenu=data[nombreEleve*3:nombreEleve*3+8]
 notesOptions=data[nombreEleve*3+8:nombreEleve*3+16]
+
+nbEleveParOption=[]
+for i in range(nombreOptions):
+    nbEleveParOption.append(0)
 
 resultat=[[],[],[],[]]
 
@@ -15,14 +23,17 @@ for i in range(round(nombreEleve)):
     resultat[0].append(eleve[i]);
     resultat[2].append(choixObtenu[i])
     resultat[3].append(notesOptions[i])
-    if(data[i*3] == "1"):
-        resultat[1].append("LD")
-    if (data[i * 3+1] == "1"):
-        resultat[1].append("SE")
-    if (data[i * 3+2] == "1"):
-        resultat[1].append("BIO")
+    for j in range(nombreOptions):
+        if(data[i*nombreOptions+j]== "1"):
+            resultat[1].append(options[j])
+            nbEleveParOption[j]+=1
+
 
 resultat= np.array(resultat)
 resultat=np.transpose(resultat)
 
+for i in range(nombreOptions):
+    print("Il y a "+str(nbEleveParOption[i])+" étudiant dans l'option "+ options[i])
 print(resultat)
+
+
