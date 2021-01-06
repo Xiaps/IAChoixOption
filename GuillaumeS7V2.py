dd9ecd3b328016903a4e21513f50e192607b1c7e
Guillaume <guideb21@orange.fr>
2021-01-06 10:20:11 +0100

    minimize working

--- GuillaumeS7V2.py
+++ GuillaumeS7V2.py
@@ -1,37 +1,31 @@
 from pycsp3 import *
-#Les options
+
+# Les options
 options = LD, SE, BIO = 0, 1, 2
-#Les places de optionss
+
+# Les places de optionss
 optionsPlaces = [4, 4, 2]
-#Les eleves
-Guillaume, Maxime, Alban, Roger, Manu, Morgane, Vegeta, Clara = eleves = VarArray(size=8, dom=options)
-choix = VarArray(size=8,dom=range(3))
-#Set de préférence pour chaque eleve
-elevesPrefs = [[2, 1, 0], [0, 1, 2], [0, 1, 2], [1, 2, 0], [1, 2, 0], [0, 2, 1], [1, 0, 2], [1, 0, 2]]
-#La suite n'est pas encore utilise (jusqu'au satisfy)
-#Set de notes pour chaque eleve
-elevesNotes = [[15, 15], [12, 18], [20, 4], [12, 13], [9, 8], [17, 18], [5, 19], [16, 11]]
-#Set de coefs des notes pour chaque option
-coefsOptions = [[2, 1], [1, 1], [1, 3]]
-#Set de moyennes coeficientés par option pour chaque eleve
-elevesMoyenneOptions = [[], [], [], [], [], [], [], []]
-#Calcul des moyennes coeficcientees pour chaque eleve
-for y in range(len(options)):
-    for i in range(len(eleves)):
-        moyenne = 0
-        for x in range(len(elevesNotes[0])):
-            moyenne += elevesNotes[i][x] * coefsOptions[y][x]
-        moyenne = round(moyenne / sum(coefsOptions[y]), 2)
-        elevesMoyenneOptions[i].append(moyenne)
-#Conditions : (Commentaires hors du satisfy sinon erreur chez moi, raison inconnue)
-# Il ne peut pas y avoir plus d'eleves par option que de place disponible
-# Un eleve doit être attribue dans l'option de son 1er choix sauf si celle ci est pleine. (Donc soit il valide que son opption est son choix 1 soit que cette option est pleine.)
+
+# Les eleves
+eleves = VarArray(size=[8,3], dom={0, 1})
+
+# Var choix
+choix = VarArray(size=8, dom=range(3))
+
+# Set de choix pour chaque eleve
+elevesChoix = [[2, 1, 0], [0, 1, 2], [0, 1, 2], [1, 2, 0], [1, 2, 0], [0, 2, 1], [1, 0, 2], [1, 0, 2]]
+
+# table
+#table = {(i, elevesChoix[i]) for i in range(eleves)}
+
 satisfy(
-    [Count(eleves, value=options[i]) <= optionsPlaces[i] for i in range(len(options))],
-     [choix[i]==elevesPrefs[i].index(eleves[i]) for i in range(len(eleves))]
+    #[Count(eleves, value=options[i]) <= optionsPlaces[i] for i in range(len(options))]
+
+    [choix[i]==Sum(eleves[i]*elevesChoix[i]) for i in range(8)],
+    [Sum(eleves[i])==1 for i in range(8)]
+    #[(eleves[i], choix[i]) in table for i in range(eleves)]
 )
+
 minimize(
     Sum(choix)
-    #Sum(elevesChoix[i].index(eleves[i]) for i in range(len(eleves)))
-    #Sum(i for i in range(len(eleves)))
 )
