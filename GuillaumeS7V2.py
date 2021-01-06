d4483db5988663fab6b83a3c0bb8e877612da094
Guillaume <guideb21@orange.fr>
2021-01-06 10:31:47 +0100

    minimize+satisfy

--- GuillaumeS7V2.py
+++ GuillaumeS7V2.py
@@ -12,20 +12,20 @@ eleves = VarArray(size=[8,3], dom={0, 1})
 # Var choix
 choix = VarArray(size=8, dom=range(3))
 
-# Set de choix pour chaque eleve
+# Set de choix pour chaque eleve, l'option 1ere est indiqué par 0,n la deuxieme par 1 etc etc...
 elevesChoix = [[2, 1, 0], [0, 1, 2], [0, 1, 2], [1, 2, 0], [1, 2, 0], [0, 2, 1], [1, 0, 2], [1, 0, 2]]
 
-# table
-#table = {(i, elevesChoix[i]) for i in range(eleves)}
 
 satisfy(
-    #[Count(eleves, value=options[i]) <= optionsPlaces[i] for i in range(len(options))]
-
+    [Sum(eleves[x][i] for x in range(8)) <= optionsPlaces[i] for i in range(len(options))],
     [choix[i]==Sum(eleves[i]*elevesChoix[i]) for i in range(8)],
     [Sum(eleves[i])==1 for i in range(8)]
-    #[(eleves[i], choix[i]) in table for i in range(eleves)]
 )
 
 minimize(
     Sum(choix)
+)
+
+maximize(
+
 )
