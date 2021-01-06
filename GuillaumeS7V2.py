5597dc1976a5b0e9ff635a960ce3d8fbe17fc9cc
Maxime Belouin <maxime.belouin@reseau.eseo.fr>
2021-01-06 14:40:25 +0100

    Ajout d'une option

--- GuillaumeS7V2.py
+++ GuillaumeS7V2.py
@@ -1,16 +1,16 @@
 from pycsp3 import *
 
 # Les options
-options = LD, SE, BIO = 0, 1, 2
+options = LD, SE, BIO, CSS = 0, 1, 2, 3
 
 # Les places de optionss
-optionsPlaces = [4, 4, 2]
+optionsPlaces = [4, 4, 2, 2]
 
 # Les eleves
-elevesV = VarArray(size=[8,3], dom={0, 1})
+elevesV = VarArray(size=[8,4], dom={0, 1})
 
 # Var choix
-choixV = VarArray(size=8, dom=range(3))
+choixV = VarArray(size=8, dom=range(4))
 
 #var moyenne
 moyenneV = VarArray(size=8, dom=range(20))
@@ -19,13 +19,13 @@ moyenneV = VarArray(size=8, dom=range(20))
 score = Var(dom=range(10000))
 
 # Set de choix pour chaque eleve, l'option 1ere est indiqué par 0,n la deuxieme par 1 etc etc...
-elevesChoix = [[2, 1, 0], [0, 1, 2], [0, 1, 2], [1, 2, 0], [1, 2, 0], [0, 2, 1], [1, 0, 2], [1, 0, 2]]
+elevesChoix = [[2, 1, 0, 3], [2, 1, 3, 0], [0, 1, 2, 3], [1, 2, 3, 0], [1, 2, 0, 3], [0, 2, 1, 3], [1, 0, 2, 3], [1, 3, 2, 0]]
 
 #Set de notes pour chaque eleve
 elevesNotes = [[15, 15], [12, 18], [20, 4], [12, 13], [9, 8], [17, 18], [5, 19], [16, 11]]
 
 #Set de coefs des notes pour chaque option
-coefsOptions = [[2, 1], [1, 1], [1, 3]]
+coefsOptions = [[2, 1], [1, 1], [1, 3], [1, 1]]
 
 #Set de moyennes coeficientés par option pour chaque eleve
 elevesMoyenneOptions = [[], [], [], [], [], [], [], []]
